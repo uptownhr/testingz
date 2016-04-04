fa4894c6a193885a80a618423f0cb67323183d87
mochacat <cat.r.mocha@gmail.com>
2016-04-04 11:57:17 -0700

    fixes #5

--- controllers/User.js
+++ controllers/User.js
@@ -11,4 +11,30 @@ router.get('/account', function(req,res){
   }})
 })
 
+router.post('/account', function(req,res,next){
+  const body = req.body;
+  var name = body.first_name ? body.first_name + ' ' + body.last_name : req.user.name;
+  var email = body.email ? body.email : req.user.email;
+
+  if (body.email)
+    req.assert('email', 'Email is not valid').isEmail();
+  
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/user/account');
+  }
+
+  User.update({_id: req.user._id}, { profile : {name: name}, email : email}, function(err,doc){
+    if (err){
+      req.flash('errors', {msg: 'Error: Could not update.'});
+      return res.redirect('/user/account');
+    } else {
+      req.flash('success', { msg: 'Success! Account updated!' });
+      return res.redirect('/user/account')
+    }
+  })    
+})
+
 module.exports = router
