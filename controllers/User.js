974dc8f05154aa407e5267048a3c0d805e7890d2
James Lee <uptownhr@gmail.com>
2016-04-05 16:46:22 -0700

    updated user account page

--- controllers/User.js
+++ controllers/User.js
@@ -2,13 +2,9 @@ const router = require('express').Router(),
   User = require('../models/User');
 
 router.get('/account', function(req,res){
-  var user = req.user.profile;
- 
-  res.render('account', { query : {
-    first_name: user.name.split(' ')[0],
-    last_name: user.name.split(' ')[1],
-    email: user.email
-  }})
+  var user = req.user
+
+  res.render('account', { user } )
 })
 
 module.exports = router
