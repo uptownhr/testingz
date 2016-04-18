bb939ec75b6c12d0e4c76853dc7c3e19d747e65a
James Lee <uptownhr@gmail.com>
2016-04-18 14:04:45 -0700

    issue #62 added example to create sample data in the before hook

--- test/admin.js
+++ test/admin.js
@@ -1,6 +1,14 @@
 "use strict"
 var request = require('supertest'),
-  mongoose = require('mongoose')
+  mongoose = require('mongoose'),
+  Models = require('../models'),
+  User = Models.User,
+  Post = Models.Post,
+  Project = Models.Project,
+  Product = Models.Product,
+  File = Models.File
+
+
 var app = require('../index.js');
 
 const admin_user = {
@@ -9,26 +17,41 @@ const admin_user = {
 }
 
 // for storing session
+
 const admin = request.agent(app);
-
-describe('Admin', function(){
-  before(function(done){
-
+const adminLoggedIn = function(){
+  return new Promise( function(resolve, reject){
     /*create admin login */
     admin
       .post('/auth/login')
       .send(admin_user)
-      .expect(302, done)
+      .expect(302, function(e){
+        if(e) return reject(e)
+        return resolve()
+      })
   })
 
-  after( function(){
-    //mongoose.connection.close()
-    //app.server.close()
-  })
+}
 
+describe('Admin', function(){
+  const sample_user = User({ email: 'test@test.com', password: 'asdfasdf' })
+  const sample_post = Post({ title: ''})
+  const sample_project = Project({ })
+  const sample_product = Product({ })
+  const sample_file = File({ })
+
+  before(function(done){
+    Promise.all([
+      adminLoggedIn(),
+      sample_user.save()
+    ]).then( () => done(), function error(err){
+      done(err)
+    })
+
+  })
 
   describe('Admin not logged in', function(){
-    it('should return 303 when not admin', function(done){
+    it('should return 302 when not admin', function(done){
       request(app)
         .get('/admin/')
         .expect(302, done)
@@ -46,12 +69,22 @@ describe('Admin', function(){
   describe('GET /admin/users', function(){
     it('should return 200 OK', function(done){
       admin
-      .get('/admin/users')
-      .expect(200)
-      .then( function(){
-        console.log('yes')
-        done()
-      })
+        .get('/admin/users')
+        .expect(200)
+        .then( function(){
+          done()
+        })
+    })
+  })
+
+  describe('GET /admin/user', function(){
+    it('should return 200 OK', function(done){
+      admin
+        .get('/admin/user')
+        .expect(200)
+        .then( function(){
+          done()
+        })
     })
   })
 
@@ -77,5 +110,13 @@ describe('Admin', function(){
         .get('/admin/files')
         .expect(200, done)
     })
+  })
+
+  after( function(){
+    sample_user.remove()
+    sample_post.remove()
+    sample_project.remove()
+    sample_product.remove()
+    sample_file.remove()
   })
 })
