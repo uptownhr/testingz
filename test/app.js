10a99a3381aa30edfb78855b111ecacb07ff2fe1
James Lee <uptownhr@gmail.com>
2016-05-02 18:32:02 -0700

    issue #76 updating test app.js to es6 airbnb styles

--- test/app.js
+++ test/app.js
@@ -1,123 +1,117 @@
-var request = require('supertest'),
+const request = require('supertest'),
   chai = require('chai'),
-  expect = chai.expect,
   _ = require('lodash'),
   mongoose = require('mongoose'),
   User = require('../models/User'),
   Post = require('../models/Post'),
   Product = require('../models/Product')
 
-var app = require('../index.js')
+const expect = chai.expect
+const app = require('../index.js')
 
 const user = request.agent(app)
-const user_info = {
+const USER_INFO = {
   email: 'admin@admin.com',
   password: 'asdfasdf'
 }
 
-const test_user = User({email: 'asdf@asdf.com', password:'asdf'})
-test_user.save()
+const TEST_USER = User({ email: 'asdf@asdf.com', password: 'asdf' })
 
-describe( 'App', function(){
-  before(function(done){
-
+describe('App', () => {
+  before(done => {
     user
       .post('/auth/login')
-      .send(user_info)
+      .send(USER_INFO)
       .expect(302, done)
   })
 
-  describe('GET /', function() {
-    it('should return 200 OK', function(done) {
+  before(done => {
+    TEST_USER.save(done)
+  })
+
+  describe('GET /', () => {
+    it('should return 200 OK', done => {
       request(app)
         .get('/')
         .expect(200, done);
     });
   });
 
-  describe('GET /auth/login', function() {
-    it('should return 200 OK', function(done) {
+  describe('GET /auth/login', () => {
+    it('should return 200 OK', done => {
       request(app)
         .get('/auth/login')
         .expect(200, done);
     });
   });
-  
-  describe('GET /auth/logout', function() {
-    it('should return 302 redirect', function(done){
+
+  describe('GET /auth/logout', () => {
+    it('should return 302 redirect', done => {
       request(app)
-      .get('/auth/logout')
-      .expect(302, done)
+        .get('/auth/logout')
+        .expect(302, done)
     })
   });
 
-  describe('GET /auth/register', function() {
-    it('should return 200 OK', function(done) {
+  describe('GET /auth/register', () => {
+    it('should return 200 OK', done => {
       request(app)
         .get('/auth/register')
         .expect(200, done);
     });
   });
-  
-  describe('GET /auth/register', function() {
-    it('should return 200 OK', function(done) {
+
+  describe('GET /auth/o/twitter', () => {
+    it('should return 302 redirect', done => {
       request(app)
-        .get('/auth/register')
-        .expect(200, done);
-    });
-  });
-  
-  describe('GET /auth/o/twitter', function() {
-    it('should return 302 redirect', function(done){
-      request(app)
-      .get('/auth/o/twitter')
-      .expect(302, done)
+        .get('/auth/o/twitter')
+        .expect(302, done)
     })
   });
-  
-  describe('GET /auth/o/twitter/callback', function() {
-    it('should return 302 redirect', function(done){
+
+  describe('GET /auth/o/twitter/callback', () => {
+    it('should return 302 redirect', done => {
       request(app)
-      .get('/auth/o/twitter/callback')
-      .expect(302, done)
+        .get('/auth/o/twitter/callback')
+        .expect(302, done)
     })
   });
-  
-  describe('GET /auth/o/testing/', function() {
-    it('should return 302 redirect', function(done){
+
+  describe('GET /auth/o/testing/', () => {
+    it('should return 302 redirect', done => {
       request(app)
-      .get('/auth/o/testing')
-      .expect(302, done)
+        .get('/auth/o/testing')
+        .expect(302, done)
     })
   });
-  
-  describe('GET /auth/o/testing/callback', function() {
-    it('should return 302 redirect', function(done){
+
+  describe('GET /auth/o/testing/callback', () => {
+    it('should return 302 redirect', done => {
       request(app)
-      .get('/auth/o/testing/callback')
-      .expect(302, done)
+        .get('/auth/o/testing/callback')
+        .expect(302, done)
     })
   });
-  
-  describe('GET /random-url', function() {
-    it('should return 404', function(done) {
+
+  describe('GET /random-url', () => {
+    it('should return 404', done => {
       request(app)
         .get('/reset')
         .expect(404, done);
     });
   });
 
-  describe('GET /blog', function(){
-    it('should return 200', function(done){
+  describe('GET /blog', () => {
+    it('should return 200', done => {
       request(app)
         .get('/blog')
         .expect(200, done)
     })
   })
-  
-  describe('GET /blog/post/:id', function(){
-    it('should return 200', function(done) {
-      Post.findOne({ title: 'First Blog Post' }, function(err, post) {
+
+  describe('GET /blog/post/:id', () => {
+    it('should return 200', done => {
+      Post.findOne({ title: 'First Blog Post' }, (err, post) => {
         var url = '/blog/post/' + post._id;
         request(app)
           .get(url)
@@ -125,88 +119,87 @@ describe( 'App', function(){
       });
     });
   });
-  
-  describe('GET /user/account', function(){
-    it('should return 300 unauthorized', function(done){
+
+  describe('GET /user/account', () => {
+    it('should return 300 unauthorized', done => {
       request(app)
         .get('/user/account')
         .expect(302, done)
     })
 
-    it('should return 200 when authorized', function(done){
+    it('should return 200 when authorized', done => {
       user
         .get('/user/account')
-        .expect(200,done)
+        .expect(200, done)
     })
   })
-  
-  describe('GET /user/account/unlink/testing', function(){
-    it('should return 302', function(done){
+
+  describe('GET /user/account/unlink/testing', () => {
+    it('should return 302', done => {
       request(app)
         .get('/user/account/unlink/testing')
         .expect(302, done)
     })
-    
+
   });
 
-  describe('GET /user/account/unlink/mock_provider', function(){
-    it('should return 302', function(done){
+  describe('GET /user/account/unlink/mock_provider', () => {
+    it('should return 302', done => {
       user
         .get('/user/account/unlink/mock_provider')
         .expect(302, done)
     })
-    
-    it('should not find the mock_provider', function(done) {
-      User.findOne({ email: user_info.email }, function(err, user) {
+
+    it('should not find the mock_provider', done => {
+      User.findOne({ email: USER_INFO.email }, (err, user) => {
         if (err) return done(err);
         var providers = _.map(user.providers, 'name');
         expect('mock_provider').to.not.be.oneOf(providers);
         done();
       });
     });
-    
+
   });
 
-  describe('POST /user/account', function(){
-    it('should 302 redirect when invalid email is given', function(done){
+  describe('POST /user/account', () => {
+    it('should 302 redirect when invalid email is given', done => {
       user.post('/user/account')
-      .send({email: 'asdf'})
-      .expect(302, done)
+        .send({ email: 'asdf' })
+        .expect(302, done)
     })
 
-    it('should 302 redirect to /user/accounts when valid email address given', function(done){
+    it('should 302 redirect to /user/accounts when valid email address given', done => {
       const email = 'zzzz@aol.com'
 
       user.post('/user/account')
-        .send({email})
-        .expect(302, function(e, res){
-          if(e) return done(e)
+        .send({ email })
+        .expect(302, (e, res) => {
+          if (e) return done(e)
           res.header.location.should.equal('/user/account')
-          User.findOne({email}, function(err, found){
+          User.findOne({ email }, (err, found) => {
             found.should.not.be.null
             done()
           })
         })
     })
 
-    it('should 302 redirect and not update if email is taken', function(done){
+    it('should 302 redirect and not update if email is taken', done => {
       const email = 'asdf@asdf.com'
       user.post('/user/account')
-        .send({email})
-        .expect(302, function(e){
-          User.findOne({email}, function(err, user){
-            user._id.should.eql(test_user._id)
+        .send({ email })
+        .expect(302, e => {
+          User.findOne({ email }, (err, user) => {
+            user._id.should.eql(TEST_USER._id)
             done()
           })
         })
     })
   })
 
-
-  describe('POST /charge', function() {
-    it('should return 200 OK', function (done) {
-      Product.findOne(function (err, product) {
-        const req = {stripeToken: "gordo", id: product._id}
+  describe('POST /charge', () => {
+    it('should return 200 OK', done => {
+      Product.findOne((err, product) => {
+        const req = { stripeToken: 'gordo', id: product._id }
         request(app)
           .post('/charge')
           .send(req)
@@ -215,32 +208,36 @@ describe( 'App', function(){
     })
   })
 
-  describe('POST /auth/login', function(){
-    it('should not log user in if email not found', function(done){
+  describe('POST /auth/login', () => {
+    it('should not log user in if email not found', done => {
       user.get('/auth/logout')
-        .then( function(){
-          return user.post('/auth/login').send({email: 'zzzzzz'})
-            .expect(302)
-
-        })
-        .then( function(){
-          user
-            .get('/user/account')
-            .expect(302, done)
-        })
+        .then(() => (
+            user
+              .post('/auth/login')
+              .send({ email: 'zzzzzz' })
+              .expect(302)
+          )
+        )
+        .then(() => (
+            user
+              .get('/user/account')
+              .expect(302, done)
+          )
+        )
     })
 
-    it('should not log user in if bad password', function(done){
+    it('should not log user in if bad password', done => {
       user.get('/auth/logout')
-        .expect(302, function(){
-          return user.post('/auth/login').send({email: 'admin@admin.com'})
-            .expect(302, function(){
+        .expect(302, () => (
+          user
+            .post('/auth/login').send({ email: 'admin@admin.com' })
+            .expect(302, () => (
               user
                 .get('/admin/')
                 .expect(302, done)
-            })
+            ))
 
-        })
+        ))
     })
   })
 })
