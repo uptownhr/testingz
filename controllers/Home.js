a41cc9bada8e5a7b0a31dc40ffc2ab8f1915959d
mochacat <cat.r.mocha@gmail.com>
2016-05-04 14:13:32 -0700

    converted async control flow for Home.js to bluebird promises

--- controllers/Home.js
+++ controllers/Home.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const async = require('async'),
+  Promise = require('bluebird'),
   config = require('../config'),
   Project = require('../models/Project'),
   Product = require('../models/Product');
@@ -7,52 +8,38 @@ const async = require('async'),
 const stripe = require('stripe')(config.payment.stripe.secret_key);
 router.get('/', function (req, res) {
 
-  async.parallel({
-    projects: function (callback) {
-      Project.find({}).exec(callback);
-    },
-
-    products: function (callback) {
-      Product.find({}).exec(callback);
-    }
-  }, function (err, result) {
-    result.stripe = config.payment.stripe.public_key;
-    res.render('index', result);
-  });
-
+  const projects = Project.find({}).exec()
+  const products = Product.find({}).exec()
+  
+  Promise.all([projects, products]).then(docs => {
+    res.render('index', {
+      projects: docs[0],
+      products: docs[1],
+      stripe: config.payment.stripe.public_key
+    })
+  })
 })
 
 router.post('/charge', function (req, res) {
-    var stripeToken = req.body.stripeToken;
+  const stripeToken = req.body.stripeToken
 
-    async.waterfall([
-      function (callback) {
-        Product.findOne({ _id: req.body.id }, function (err, product) {
-          callback(null, product);
-        });
-      },
-
-      function (product, callback) {
-        var charge = stripe.charges.create({
-          amount: product.price * 100, //cents
-          description: product.name,
-          currency: 'usd',
-          source: stripeToken
-        }, function (err, charge) {
-          callback(err, charge);
-        });
-      }
-    ], function (err, charge) {
-      if (err && err.type === 'StripeCardError') {
+  Product.findOne({ _id: req.body.id }).exec().then(product => {
+    var charge = stripe.charges.create({
+      amount: product.price * 100, //cents
+      description: product.name,
+      currency: 'usd',
+      source: stripeToken
+    }, function (err, charge) {
+      if (err && err.type == 'StripeCardError') {
         req.flash('error', { msg: 'Error: Card has been declined.' });
       } else if (err) {
         req.flash('error', { msg: 'Error: Payment did not go through.' });
       } else {
         req.flash('success', { msg: 'Success: Payment has been accepted.' });
       }
-
       res.redirect('/');
     });
-  })
+  }).catch(e => console.log(err))
+})
 
 module.exports = router
