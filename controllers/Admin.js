4c98388fa9dc79f9808f576d9a066328281fd5d9
Vick <eapvick@gmail.com>
2016-05-08 20:37:22 -0700

    added admin views for contact lists and contact edit, had to hard code some jade temporarily to see output, will wire models to it later with James help

--- controllers/Admin.js
+++ controllers/Admin.js
@@ -9,11 +9,13 @@ const
   models = require('../models')
 
 const upload = multer({ dest: path.join(__dirname, '../public/uploads') })
+// Get Model Schemas
 const User =  models.User
 const Post = models.Post
 const File = models.File
 const Project = models.Project
 const Product = models.Product
+const Contact = models.Contact
 
 router.get('/', function (req, res) {
   res.render('admin/overview')
@@ -500,6 +502,15 @@ router.get('/file/delete/:id', function (req, res) {
   })
 })
 
+// route to contacts list view in admin ctrl panel 
+router.get('/contacts', function(req, res) {
+  res.render('admin/contacts.jade')
+})
+
+// route to individual contact view/edit in admin ctrl panel
+router.get('/contact', function(req, res) {
+  res.render('admin/contact.jade')
+})
 
 
 module.exports = router
--- controllers/Home.js
+++ controllers/Home.js
@@ -3,6 +3,7 @@ const async = require('async'),
   Promise = require('bluebird'),
   config = require('../config'),
   Project = require('../models/Project'),
+  Contact = require('../models/Contact'),
   Product = require('../models/Product');
 
 const stripe = require('stripe')(config.payment.stripe.secret_key);
@@ -46,5 +47,40 @@ router.post('/charge', function (req, res) {
 router.get('/contact', function (req, res) {
   res.render('contact.jade');
 })
+
+/**** Code Vick wrote over weekend, need to double check *****/
+// Currently commented out because uncommenting breaks the app. 
+// Keeping code for further analysis by James 
+// POST Saving Individual Contact Info to Mongo
+/*router.post('/admin/contact', function (req, res) {
+  var id = req.body.id;
+  var body = req.body;
+  Contact.findOne({ _id: id }, function(err, contact)) {
+    if (contact) {
+        contact.name = body.name;
+        contact.email = body.email;
+        contact.message = body.message;
+        contact.created = body.created;
+        contact.save(function(err, saved) {
+            res.redirect('/admin/contacts');
+        }) //end of save method
+    } //end of if clause
+    else { // new contact instance
+      var contact = new Contact({
+        name: body.name;
+        email: body.email;
+        message: body.message;
+        created: body.created;
+      })
+      contact.save(function(err, saved) {
+        res.redirect('/admin/examples')
+      }) 
+    }
+  }
+})*/ //end of post request
+
+
+/**** End of Vick's code *****/
+
 
 module.exports = router
